b48cc962e8ae333357a174e91dceb1e7a630f498
hlgirard <hl.girard@gmail.com>
2019-03-15 10:34:11 -0400

    Updated setup.py to require opencv

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='xptools',
       license='MIT',
       packages=find_packages(),
       install_requires=[
+          'opencv-python'
           'numpy',
           'pandas',
           'scikit-image',
